f2414b441363253442ee2ad1b4963d39ef1f32ea
ReneRojas25 <84557114+ReneRojas25@users.noreply.github.com>
2021-05-27 00:15:39 -0500

    Add files via upload

---
+++ Ejemplo8.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title> Ejemplo 8 </title>
+</head>
+<body><?php $num=rand(1,100);
+echo $num;
+echo "<br>";
+if ($num<=50){
+echo "El número es menor o igual a 50.";
+}
+else{
+echo "El número es mayor a 50.";
+}?>
+<a href="sub2.html">Regresar</a></body</html>
